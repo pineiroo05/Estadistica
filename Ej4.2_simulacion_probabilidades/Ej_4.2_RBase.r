d4089e8b873e4b2961575bcb95a21d6c101e5c48
Enrique🐬 <enripl05@gmail.com>
2025-09-25 19:33:15 +0200

    Add files via upload

---
+++ Ej4.2_simulacion_probabilidades/Ej_4.2_RBase.r
@@ -0,0 +1,16 @@
+directorio<-readline(prompt="Escribe el directorio donde tienes el data frame")
+setwd(directorio)
+load("dataframe_ej4.2.RData")
+
+#Aqui vendria la recodificacion para crear las variables x de cada jugador. Como en R base no hay recodificar uso una estructura ifeslse
+UniformSamples$x1 <- ifelse(UniformSamples$obs1 <= 0.8, 0, 1)
+UniformSamples$x2 <- ifelse(UniformSamples$obs2 <= 0.9, 0, 1)
+UniformSamples$x3 <- ifelse(UniformSamples$obs3 <= 0.6, 0, 1)
+UniformSamples$x4 <- ifelse(UniformSamples$obs4 <= 0.5, 0, 1)
+UniformSamples$x5 <- ifelse(UniformSamples$obs5 <= 0.7, 0, 1)
+
+#Ahora habria q crear las variables z1, z2 y z3
+UniformSamples$z1 <- with(UniformSamples, x1 | x2)
+UniformSamples$z2 <- with(UniformSamples, x3 | x4)
+UniformSamples$z3 <- with(UniformSamples, z1 & z2)
+View(UniformSamples)
