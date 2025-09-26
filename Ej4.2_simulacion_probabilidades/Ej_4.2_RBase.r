825a228e0a116535a8c191a0b65a607707a89412
Enrique🐬 <enripl05@gmail.com>
2025-09-26 16:19:21 +0200

    Add files via upload

--- Ej4.2_simulacion_probabilidades/Ej_4.2_RBase.r
+++ Ej4.2_simulacion_probabilidades/Ej_4.2_RBase.r
@@ -1,11 +1,16 @@
 load("dataframe_ej4.2.RData")
 
 #Aqui vendria la recodificacion para crear las variables x de cada jugador. Como en R base no hay recodificar uso una estructura ifeslse
-UniformSamples$x1 <- ifelse(UniformSamples$obs1 <= 0.8, 0, 1)
-UniformSamples$x2 <- ifelse(UniformSamples$obs2 <= 0.9, 0, 1)
-UniformSamples$x3 <- ifelse(UniformSamples$obs3 <= 0.6, 0, 1)
-UniformSamples$x4 <- ifelse(UniformSamples$obs4 <= 0.5, 0, 1)
-UniformSamples$x5 <- ifelse(UniformSamples$obs5 <= 0.7, 0, 1)
+#UniformSamples$x1 <- ifelse(UniformSamples$obs1 <= 0.8, 0, 1), esta sería la primera opcion, pero queda mal pq habria que hacerlo 5 veces # nolint
+
+var <- c("obs1", "obs2", "obs3", "obs4", "obs5")
+probs <- c(0.8, 0.9, 0.6, 0.5, 0.7)
+
+UniformSamples[paste0("x", 1:5)]<-mapply(
+    function(v, p) ifelse(UniformSamples[[v]] <= p, 0, 1),
+    v=var,
+    p=probs
+)
 
 #Ahora habria q crear las variables z1, z2 y z3
 UniformSamples$z1 <- with(UniformSamples, x1 | x2)
