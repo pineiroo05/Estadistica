26c178eae03ba2a949e0689df9c59766a7e6f208
Enrique🐬 <enripl05@gmail.com>
2025-10-09 21:14:58 +0200

    Add files via upload

--- Ej4.3_simulacion_probabilidades/Ej4.3_Rbase.r
+++ Ej4.3_simulacion_probabilidades/Ej4.3_Rbase.r
@@ -3,13 +3,45 @@ load("UniformSamples.RData")
 #Hay que recodificar para crear las variables LBx1, LBx2...
 
 #LARRY BIRD
-var_lb <- c("LB1", "LB2", "LB3", "LB4", "LB5")
+#En este caso todas las columnas q genera nuevas, salen con N/A, pq el dataframe "no esta bien", es decir no tengo los LB1B1 por ej, solo tengo los LB1, LB2...
+#Tendria tb q ver si me conviene mas un lapply o mapply
+
+#var lbx -> la x indica la posicion
+var_lb1 <- paste0("LB1B", 1:5)
+var_lb2 <- paste0("LB2B", 1:5)
+var_lb3 <- paste0("LB3B", 1:5)
+var_lb4 <- paste0("LB4B", 1:5)
+var_lb5 <- paste0("LB5B", 1:5)
+#Son las probabilidades de acierto desde cada posicion
 probs_lb <- c(0.356, 0.376, 0.369, 0.376, 0.356)
 
-UniformSamples[paste0("LBx", 1:5)] <- mapply(
+UniformSamples[paste0("LBx1B", 1:5)] <- lapply(
+    var_lb1,
+    function(v) ifelse(UniformSamples[[v]] <= 0.356, 0, 1)
+)
+
+UniformSamples[paste0("LBx2B", 1:5)] <- mapply(
     function(v, p) ifelse(UniformSamples[[v]] <= p, 0, 1),
-    v = var_lb,
-    p = probs_lb
+    v = var_lb2,
+    p = probs_lb[2]
+)
+
+UniformSamples[paste0("LBx3B", 1:5)] <- mapply(
+    function(v, p) ifelse(UniformSamples[[v]] <= p, 0, 1),
+    v = var_lb3,
+    p = probs_lb[3]
+)
+
+UniformSamples[paste0("LBx4B", 1:5)] <- mapply(
+    function(v, p) ifelse(UniformSamples[[v]] <= p, 0, 1),
+    v = var_lb4,
+    p = probs_lb[4]
+)
+
+UniformSamples[paste0("LBx5B", 1:5)] <- mapply(
+    function(v, p) ifelse(UniformSamples[[v]] <= p, 0, 1),
+    v = var_lb5,
+    p = probs_lb[5]
 )
 
 #CRAIG HODGES
@@ -33,10 +65,10 @@ UniformSamples[paste0("DBx", 1:5)] <- mapply(
 )
 
 #Ahora las z
-UniformSamples$z1 <- with(UniformSamples, LBx1 & LBx2 & LBx3 & LBx4 & LBx5)
-UniformSamples$z2 <- with(UniformSamples, CHx1 & CHx2 & CHx3 & CHx4 & CHx5)
-UniformSamples$z3 <- with(UniformSamples, DBx1 & DBx2 & DBx3 & DBx4 & DBx5)
+#UniformSamples$z1 <- with(UniformSamples, LBx1 & LBx2 & LBx3 & LBx4 & LBx5)
+#UniformSamples$z2 <- with(UniformSamples, CHx1 & CHx2 & CHx3 & CHx4 & CHx5)
+#UniformSamples$z3 <- with(UniformSamples, DBx1 & DBx2 & DBx3 & DBx4 & DBx5)
 
-UniformSamples$z4 <- with(UniformSamples, z1 | z2 | z3)
+#UniformSamples$GANADOR <- with(UniformSamples, z1 | z2 | z3)
 
 View(UniformSamples)
